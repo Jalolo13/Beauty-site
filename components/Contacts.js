e3ce7726e16bd0bcdf28587bdcb854564dfb35c0
Jalolo13 <jalololo13@gmail.com>
2022-08-22 03:25:38 +0600

    did map

--- components/Contacts.js
+++ components/Contacts.js
@@ -10,16 +10,24 @@ const Contacts = () => {
                     <p className={styles.contacts__text}>
                         м. Тульская, Самаринская ул. 1
                     </p>
+                    <a href="https://yandex.ru/maps/-/CCURnPVy0B">
+                        Проложить маршрут
+                    </a>
                 </div>
                 <div className={styles.contacts__block}>
                     <h4 className={styles.contacts__subtitle}>Телефон</h4>
-                    <p className={styles.contacts__text}>+7 (995) 099-27-57</p>
+                    <a
+                        className={styles.contacts__text}
+                        href="tel:+79950992757"
+                    >
+                        +7 (995) 099-27-57
+                    </a>
                 </div>
 
                 <div className={styles.contacts__block}>
                     <h4 className={styles.contacts__subtitle}>Время работы</h4>
                     <p className={styles.contacts__text}>
-                        по предварительной <br />
+                        По предварительной <br />
                         записи
                     </p>
                 </div>
@@ -38,11 +46,10 @@ const Contacts = () => {
             <div className={styles.contacts__map}>
                 <h4 className={styles.contacts__subtitle}>Карта</h4>
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2246.3271194542403!2d37.52146301590019!3d55.735445080548736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54bfaa3794a45%3A0x6e485ab80c85dde!2zMTgxMiDQk9C-0LTQsCDRg9C7LiwgMSwg0JzQvtGB0LrQstCwLCDQoNC-0YHRgdC40Y8sIDEyMTE3MA!5e0!3m2!1sru!2skg!4v1660830225685!5m2!1sru!2skg"
+                    src="https://yandex.ru/map-widget/v1/-/CCURnPVy0B"
+                    frameBorder="1"
+                    allowFullScreen="true"
                     className={styles.contacts__iframe_map}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"
                 ></iframe>
             </div>
         </section>
